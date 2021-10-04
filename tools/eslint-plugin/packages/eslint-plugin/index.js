a40741e905af6a75b944512e7a86710ee7592f5e
MiZiet <michalzietalak@o2.pl>
2021-10-04 14:20:15 +0200

    Add eslint package

--- tools/eslint-plugin/packages/eslint-plugin/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    rules: {
-        header: require('./rules/header'),
-    },
-    configs: {
-        recomended: require('./configs/recomended'),
-        react: require('./configs/react'),
-    }
-}
