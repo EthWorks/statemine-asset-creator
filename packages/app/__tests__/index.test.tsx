b5d17aa5d630dba51496475acad2259f2a8b690a
MiZiet <michalzietalak@o2.pl>
2021-10-04 14:30:42 +0200

    Tests

--- packages/app/__tests__/index.test.tsx
+++ packages/app/__tests__/index.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Home from '../pages/index'
 
-jest.mock('../src/hooks/useBalances', () => ({
+jest.mock('use-substrate', () => ({
   useBalances: () => ({
     freeBalance: 3600,
     availableBalance: 4000,
