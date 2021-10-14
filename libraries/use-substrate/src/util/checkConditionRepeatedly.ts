cffbcfb0748ea2ee3d11064a6c3245429fda25cc
Krzysztof Jelski <krzysztof@ethworks.io>
2021-10-14 11:25:26 +0200

    Make condition a lambda

--- libraries/use-substrate/src/util/checkConditionRepeatedly.ts
+++ libraries/use-substrate/src/util/checkConditionRepeatedly.ts
@@ -1,12 +1,11 @@
-export const checkConditionRepeatedly = (condition: boolean, onSuccess: () => void, onFail: () => void, options?: {interval: number, timeout: number}): () => void => {
+export const checkConditionRepeatedly = (condition: () => boolean, onSuccess: () => void, onFail: () => void, options?: {interval: number, timeout: number}): () => void => {
   const interval = options?.interval || 20
   const timeout = options?.timeout || 1000
 
   let timeElapsed = 0
 
   const intervalId = setInterval(() => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    if (condition) {
+    if (condition()) {
       clearInterval(intervalId)
       onSuccess()
     } else {
