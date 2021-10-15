8430c94e730a197f54c4473fa46022054de6627e
MiZiet <michalzietalak@o2.pl>
2021-10-15 13:36:26 +0200

    Add IdProvider

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -1,17 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import dynamic from 'next/dynamic'
+import { IdProvider } from '@radix-ui/react-id'
 
 const AccountsContextProvider = dynamic(() => import('use-substrate').then((module) => module.AccountsContextProvider) as Promise<() => (JSX.Element | null)>,{ ssr: false })
 const ApiContextProvider = dynamic(() => import('use-substrate').then((module) => module.ApiContextProvider) as Promise<() => (JSX.Element | null)>,{ ssr: false })
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ApiContextProvider>
-      <AccountsContextProvider>
-        <Component {...pageProps} />
-      </AccountsContextProvider>
-    </ApiContextProvider>
+    <IdProvider>
+      <ApiContextProvider>
+        <AccountsContextProvider>
+          <Component {...pageProps} />
+        </AccountsContextProvider>
+      </ApiContextProvider>
+    </IdProvider>
   )
 }
 
