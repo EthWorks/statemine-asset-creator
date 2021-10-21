bdcf9d48af91f757ef7d5dd0645b7faa5e262114
Ewa Kowalska <30404388+ekowalsk@users.noreply.github.com>
2021-10-21 11:13:17 +0200

    Replace switch instruction (#38)
    Co-authored-by: pawelEth <80252264+pawelEth@users.noreply.github.com>

--- packages/app/components/Card/Card.tsx
+++ packages/app/components/Card/Card.tsx
@@ -1,24 +1,21 @@
 import styled from 'styled-components'
 
+type PaddingSize = 's' | 'm' | 'l'
+
 export interface CardProps {
-  padding?: 's' | 'm' | 'l'
+  padding?: PaddingSize
 }
 
 const Card = styled.div<CardProps>`
   border-radius: ${({ theme }) => theme.borderRadius.m};
   background-color: ${({ theme }) => theme.colors.black};
-  padding: ${({ padding }) => {
-    switch (padding) {
-      case 's':
-        return '16px'
-      case 'm':
-        return '24px 32px'
-      case 'l':
-        return '40px'
-      default:
-        return '0px'
-    }
-  }};
+  padding: ${({ padding }) => padding ? paddingSizes[padding] : '0px'};
+};
 `
+const paddingSizes: Record<PaddingSize, string> = {
+  s: '16px',
+  m: '24px 32px',
+  l: '40px'
+}
 
 export default Card
