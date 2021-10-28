919ab3f4eedfe9fb034f44d5b326820f76ceba1f
Pawel <pawel.r@ethworks.io>
2021-10-28 09:22:24 +0200

    Remove button from page

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -6,7 +6,6 @@ import { useState } from 'react'
 
 import { Chains, useAccounts, useBalances } from 'use-substrate'
 
-import { ButtonPrimary } from '../components'
 import styles from '../styles/Home.module.css'
 import { ACCOUNT_SELECT_URL, activeAccountSet, CONNECT_WALLET_URL, extensionActivated, useAsync } from '../utils'
 
@@ -63,11 +62,6 @@ const Home: NextPage =  () => {
             <li key={index}>address: {account.address} name: {account.name}</li>)
           }
         </ul>
-
-        <ButtonPrimary>
-          Button
-        </ButtonPrimary>
-
       </main>
     </div>
   )
