c9c1b950c1ba46a60bebc98f45c27e4d30196b90
Ewa Kowalska <ewcia.kowalska@gmail.com>
2021-11-02 14:35:51 +0100

    Handle undefined in BalanceValue

--- packages/app/components/AccountSelect/BalanceValue.tsx
+++ packages/app/components/AccountSelect/BalanceValue.tsx
@@ -6,10 +6,14 @@ import { formatBalanceValue } from '../../formaters/formaters'
 import { Text } from '../typography'
 
 interface BalanceValueProps {
-  value: BN | string
+  value: BN | string | undefined
 }
 
-const BalanceValue = ({ value }: BalanceValueProps): React.ReactElement<BalanceValueProps> => {
+const BalanceValue = ({ value }: BalanceValueProps): React.ReactElement<BalanceValueProps> | null => {
+  if (!value) {
+    return null
+  }
+
   const balanceValue = formatBalanceValue(value)
   const integers = balanceValue.substr(0, balanceValue.length - 4)
   const decimalPlaces = balanceValue.substr(balanceValue.length - 4, 4)
