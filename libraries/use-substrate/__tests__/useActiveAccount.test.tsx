b41aade124e7d405919bd413113d1318e24a46ff
MiZiet <michalzietalak@o2.pl>
2021-11-09 11:02:49 +0100

    Set and keep state in provider

--- libraries/use-substrate/__tests__/useActiveAccount.test.tsx
+++ libraries/use-substrate/__tests__/useActiveAccount.test.tsx
@@ -1,6 +1,7 @@
 import { renderHook } from '@testing-library/react-hooks'
 import React, { ReactNode, useContext } from 'react'
 
+import { BOB_ID } from '../src'
 import { ActiveAccountContext, ActiveAccountProvider, UseActiveAccount } from '../src/providers/activeAccount'
 
 function useActiveAccount(): UseActiveAccount {
@@ -8,8 +9,17 @@ function useActiveAccount(): UseActiveAccount {
 }
 
 describe('useActiveAccount', () => {
-  it('returns for no accounts in keyring', async () => {
-    renderActiveAccount()
+  beforeEach(() => {
+    localStorage.clear()
+  })
+
+  it('can set and get activeAccount via hook', async () => {
+    const { result } = renderActiveAccount()
+
+    const { setActiveAccount, activeAccount } = result.current
+    setActiveAccount(BOB_ID)
+
+    expect(activeAccount).toEqual(BOB_ID)
   })
 
   const renderActiveAccount = () => {
--- libraries/use-substrate/src/providers/activeAccount/provider.tsx
+++ libraries/use-substrate/src/providers/activeAccount/provider.tsx
@@ -1,13 +1,15 @@
-import React, { FC } from 'react'
+import { AccountId } from '@polkadot/types/interfaces'
+import React, { FC, useState } from 'react'
 
 import { ActiveAccountContext } from './context'
 import { UseActiveAccount } from './types'
 
 export const ActiveAccountProvider: FC = ({ children }) => {
+  const [activeAccount, setActiveAccount] = useState<AccountId>()
 
   const value: UseActiveAccount = {
-    activeAccount: undefined,
-    setActiveAccount: () => {/**/}
+    activeAccount: activeAccount,
+    setActiveAccount: setActiveAccount
   }
 
   return <ActiveAccountContext.Provider value={value}>{children}</ActiveAccountContext.Provider>
