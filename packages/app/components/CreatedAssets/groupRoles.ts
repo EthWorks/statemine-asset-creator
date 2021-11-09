26e2c7ed8103ec75e4d36bcddc37b4b8c7feb2af
Ewa Kowalska <ewcia.kowalska@gmail.com>
2021-11-09 10:05:26 +0100

    Refactor group roles function

--- packages/app/components/CreatedAssets/groupRoles.ts
+++ packages/app/components/CreatedAssets/groupRoles.ts
@@ -2,17 +2,33 @@ import type { AccountId } from '@polkadot/types/interfaces'
 import type { Role } from './types'
 
 export function groupRoles(admins: Record<Role, AccountId>): [AccountId, Role[]][] {
-  const result: [AccountId, Role[]][] = []
+  const groupedRoles: [AccountId, Role[]][] = []
 
   ;(Object.entries(admins) as [Role, AccountId][]).forEach(([role,account]) => {
-    const index = result.findIndex(([accountId]) => accountId.eq(account))
+    const accountIndex = findAccountsIndex(groupedRoles, account)
 
-    if (index === -1) {
-      result.push([account, [role]])
+    if (rolesNotFound(accountIndex)) {
+      initiateAccountsRoles(groupedRoles, account, role)
     } else {
-      result[index][1].push(role)
+      addRoleToGroup(groupedRoles, accountIndex, role)
     }
   })
 
-  return result
+  return groupedRoles
+}
+
+function findAccountsIndex(rolesGroupedByAccounts: [AccountId, Role[]][], account: AccountId): number {
+  return rolesGroupedByAccounts.findIndex(([accountId]) => accountId.eq(account))
+}
+
+function rolesNotFound(accountIndex: number): boolean {
+  return accountIndex === -1
+}
+
+function initiateAccountsRoles(groupedRoles: [AccountId, Role[]][], account: AccountId, role: Role): void {
+  groupedRoles.push([account, [role]])
+}
+
+function addRoleToGroup(rolesGroupedByAccounts: [AccountId, Role[]][], groupIndex: number, role: Role): void {
+  rolesGroupedByAccounts[groupIndex][1].push(role)
 }
