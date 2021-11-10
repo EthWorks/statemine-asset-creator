cd31224f9aff19e3eae5eed3097f0aa1bbac6225
Krzysztof Jelski <krzysztof@ethworks.io>
2021-11-10 14:52:39 +0100

    Patch polkadot js' detectPackage to ignore multiple imports of same library

---
+++ packages/app/__tests__/detectPackagePatch.test.ts
@@ -0,0 +1,25 @@
+import { GlobalWithPjs, patchDetectPackage } from '../utils/patchDetectPackage'
+
+function getEntry (name: string) {
+  const _global = global as GlobalWithPjs
+
+  if (!_global.__polkadotjs) {
+    _global.__polkadotjs = {}
+  }
+
+  if (!_global.__polkadotjs[name]) {
+    _global.__polkadotjs[name] = []
+  }
+
+  return _global.__polkadotjs[name]
+}
+
+describe('proxy to patch detect package', () => {
+  it('returns empty array for a chunk', () => {
+    patchDetectPackage()
+    const entry = getEntry('@polkadot/util-crypto')
+    expect(entry).toEqual([])
+    entry.push('sth')
+    expect(getEntry('@polkadot/util-crypto')).toEqual([])
+  })
+})
