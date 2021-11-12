1bcc72f09958700970ee402d27eec996a3e28bdc
Krzysztof Jelski <krzysztof@ethworks.io>
2021-11-12 11:01:51 +0100

    Shorten patch in jest config

--- libraries/use-substrate/jest-setup.ts
+++ libraries/use-substrate/jest-setup.ts
@@ -1,47 +1,16 @@
-/* eslint-disable @typescript-eslint/no-explicit-any,no-undef */
+function patchDetectPackage (): void {
+  function getObjectToPatch () {
+    if (!global.__polkadotjs) {
+      global.__polkadotjs = {}
+    }
 
-type GlobalThis = typeof globalThis;
-
-function evaluateThis (fn: (code: string) => unknown): GlobalThis {
-  return fn('return this') as GlobalThis
-}
-
-export const xglobal = (
-  typeof globalThis !== 'undefined'
-    ? globalThis
-    : typeof global !== 'undefined'
-      ? global
-      : typeof self !== 'undefined'
-        ? self
-        : typeof window !== 'undefined'
-          ? window
-          : evaluateThis(Function)
-)
-
-type This = typeof globalThis
-
-type PolkadotJsGlobal = Record<string, any>
-declare global {
-  let __polkadotjs: PolkadotJsGlobal
-}
-
-export type GlobalWithPjs = This & {__polkadotjs: PolkadotJsGlobal}
-
-const _global = xglobal as GlobalWithPjs
-
-function getObjectToPatch (): PolkadotJsGlobal {
-  if (!_global.__polkadotjs) {
-    _global.__polkadotjs = {}
+    return global.__polkadotjs
   }
 
-  return _global.__polkadotjs
-}
-export function patchDetectPackage (): void {
   if (getObjectToPatch().isProxy) {
-
     return
   }
-  _global.__polkadotjs = new Proxy(getObjectToPatch(), {
+  global.__polkadotjs = new Proxy(getObjectToPatch(), {
     get (target, key) {
       if (key === 'isProxy')
         return true
