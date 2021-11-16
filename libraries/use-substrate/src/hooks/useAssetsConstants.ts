1018f4b835788d7d1e122cf25d41ca40240a3432
miziet <michalzietalak@o2.pl>
2021-11-16 11:23:21 +0100

    Add comment for stringLimit

--- libraries/use-substrate/src/hooks/useAssetsConstants.ts
+++ libraries/use-substrate/src/hooks/useAssetsConstants.ts
@@ -5,11 +5,14 @@ import { Chains } from '../consts'
 import { useApi } from './useApi'
 
 interface UseAssetsConstants {
+  /**The maximum length of a name or symbol stored on-chain.*/
   stringLimit: BN | undefined
 }
 
 export const useAssetsConstants = (chain: Chains): UseAssetsConstants => {
   const { api, connectionState } = useApi(chain)
 
-  return useMemo(() => ({ stringLimit: api?.consts.assets.stringLimit.toBn() }), [api, connectionState])
+  return useMemo(() => ({
+    stringLimit: api?.consts.assets.stringLimit.toBn()
+  }), [api, connectionState])
 }
