b6f1d40f020d8e7059e9cdfb3406762681051164
miziet <michalzietalak@o2.pl>
2021-11-24 10:01:40 +0100

    change import to import type

--- libraries/use-substrate/src/providers/activeAccounts/context.ts
+++ libraries/use-substrate/src/providers/activeAccounts/context.ts
@@ -1,6 +1,6 @@
-import { createContext } from 'react'
+import type { UseActiveAccounts } from './types'
 
-import { UseActiveAccounts } from './types'
+import { createContext } from 'react'
 
 export const ActiveAccountsContext = createContext<UseActiveAccounts>({
   activeAccounts: {},
