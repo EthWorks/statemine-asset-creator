4093645c919aeead4269c8652a3dc834b793e3c6
miziet <michalzietalak@o2.pl>
2021-11-25 16:39:12 +0100

    Add format block number component

--- packages/app/formaters/formaters.tsx
+++ packages/app/formaters/formaters.tsx
@@ -1,12 +1,12 @@
 import { BigNumber } from 'bignumber.js'
 import BN from 'bn.js'
 
-export const roundBalance = (value: BN | string, decimalPlaces: number): string => {
+export const formatValue = (value: BN | string, decimalPlaces = 0): string => {
   return new BigNumber(value.toString()).toFormat(decimalPlaces)
 }
 
 export const shortAddress = (address: string, length = 12): string => (
   length >= address.length
     ? address
-    : address.substring(0, Math.ceil(length / 2)) + '...' +  address.substring(address.length - Math.floor(length / 2), address.length)
+    : address.substring(0, Math.ceil(length / 2)) + '...' + address.substring(address.length - Math.floor(length / 2), address.length)
 )
