f20990251781faf11ef4b5d9bd4fe957136e4b3e
Paweł Rzepiński <pawel.r@ethworks.io>
2021-11-30 11:14:19 +0100

    Info

---
+++ packages/app/components/Info/index.tsx
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+import { InfoIcon } from '../icons/InfoIcon'
+import { Text } from '../typography'
+
+interface InfoProps {
+  text: string,
+  type: 'info' | 'warning'
+}
+
+export const Info = ({ text, type }: InfoProps): JSX.Element => (
+  <InfoWrapper type={type}>
+    <div>
+      <InfoIcon />
+      <Text size='SM'>{type}</Text>
+    </div>
+    <Text size='SM' color='white'>{text}</Text>
+  </InfoWrapper>
+)
+
+const InfoWrapper = styled.div<Pick<InfoProps, 'type'>>`
+  overflow: hidden;
+  position: relative;
+  padding: 8px 16px;
+  border-radius: ${({ theme }) => theme.borderRadius.s};
+  
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: ${({ type }) => type === 'warning' ? ({ theme }) => theme.colors.red : ({ theme }) => theme.colors.blue};
+    opacity: ${({ type }) => type === 'warning' ? '.3' : '.4'};
+    z-index: -1;
+  }
+  
+  & > div {
+    display: flex;
+    align-items: center;
+    margin-bottom: 8px;
+
+    p {
+      text-transform: capitalize;
+      color: ${({ type }) => type === 'warning' ? ({ theme }) => theme.colors.redDark : ({ theme }) => theme.colors.indigo};
+    }
+
+    svg {
+      stroke: ${({ type }) => type === 'warning' ? ({ theme }) => theme.colors.redDark : ({ theme }) => theme.colors.indigo};
+      margin-right: 11px;
+    }
+  }
+`
