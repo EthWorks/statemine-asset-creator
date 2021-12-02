c95dce1d3b68e362890f58e425a2e27d88dc30ee
miziet <michalzietalak@o2.pl>
2021-12-02 13:01:31 +0100

    Rename tests

--- libraries/use-substrate/__tests__/isAddressValid.test.ts
+++ libraries/use-substrate/__tests__/isAddressValid.test.ts
@@ -6,7 +6,7 @@ import { isAddressValid } from '../src/util/checks'
 import { ALICE, ALICE_ID, BOB, BOB_ID } from './consts/addresses'
 
 describe('isAddressValid returns', () => {
-  it('true for a valid string', async () => {
+  it('true for a valid address string', async () => {
     expect(isAddressValid(BOB)).toBe(true)
     expect(isAddressValid(ALICE)).toBe(true)
   })
@@ -16,7 +16,7 @@ describe('isAddressValid returns', () => {
     expect(isAddressValid(ALICE_ID)).toBe(true)
   })
 
-  it('false for a not valid string', async () => {
+  it('false for a not valid address string', async () => {
     expect(isAddressValid(BOB.concat('9'))).toBe(false)
     expect(isAddressValid(ALICE.slice(0, ALICE.length - 2))).toBe(false)
     expect(isAddressValid('0x')).toBe(false)
