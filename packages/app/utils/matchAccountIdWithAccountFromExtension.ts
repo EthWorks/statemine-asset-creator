ff1e3412e7b78afe1e3eedde31cfecf8ebfa587e
miziet <michalzietalak@o2.pl>
2021-12-06 09:31:43 +0100

    Use convertActiveAccountToAccount in newAssetModal

--- packages/app/utils/matchAccountIdWithAccountFromExtension.ts
+++
@@ -1,6 +0,0 @@
-import type { AccountId } from '@polkadot/types/interfaces'
-import type { Account } from 'use-substrate'
-
-export function matchAccountIdWithAccountFromExtension(accountId: AccountId | undefined, extensionAccounts: Account[]): Account | undefined {
-  return extensionAccounts.find((account) => account.address === accountId?.toString())
-}
