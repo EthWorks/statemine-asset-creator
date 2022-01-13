c318ff1367223e04c5db68ffd87a40410cee64d4
MiZiet <55240109+MiZiet@users.noreply.github.com>
2022-01-13 14:40:22 +0100

    🤑 Update useInsufficientAdminBalances (#272)
    * Update useInsufficientAdminBalances
    
    * Correct calculations

--- packages/app/components/NewAssetModal/helpers/useInsufficientAdminBalances.ts
+++ packages/app/components/NewAssetModal/helpers/useInsufficientAdminBalances.ts
@@ -3,24 +3,28 @@ import type { Account } from 'use-substrate'
 
 import BN from 'bn.js'
 
-import { useBalances } from 'use-substrate'
+import { useBalances, useChainToken } from 'use-substrate'
 
 import { useAppChains } from '../../../utils'
 
 type AdminAccount = 'Admin' | 'Issuer' | 'Freezer'
 
-// to be changed based on chains decimals
-const EXPECTED_BALANCE = new BN('1000000000')
+// Unit
+const EXPECTED_BALANCE = 0.3
 
 export function useInsufficientAdminBalances(admin: Account | undefined, issuer: Account | undefined, freezer: Account | undefined): AdminAccount[] {
   const { parachain } = useAppChains()
   const { availableBalance: adminsAvailableBalance } = useBalances(admin?.address, parachain) || {}
   const { availableBalance: issuersAvailableBalance } = useBalances(issuer?.address, parachain) || {}
   const { availableBalance: freezersAvailableBalance } = useBalances(freezer?.address, parachain) || {}
+  const { chainDecimals } = useChainToken(parachain) || {}
 
+  if (chainDecimals === undefined) return []
+
+  const singleToken = new BN(10).pow(new BN(chainDecimals))
   const adminBalances: [Balance | undefined, AdminAccount][] = [[adminsAvailableBalance, 'Admin'], [issuersAvailableBalance, 'Issuer'], [freezersAvailableBalance, 'Freezer']]
 
   return adminBalances.flatMap(([balance, role]) => {
-    return balance?.lt(EXPECTED_BALANCE) ? role : []
+    return balance?.lt(singleToken.muln(EXPECTED_BALANCE)) ? role : []
   })
 }
