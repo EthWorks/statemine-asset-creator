83369b916861d440f6aeab7b8a8dbe92dd7340fd
Krzysztof Jelski <krzysztof@ethworks.io>
2022-01-27 11:07:48 +0100

    WIP 2

--- libraries/use-substrate/__tests__/deriveAssets.test.ts
+++ libraries/use-substrate/__tests__/deriveAssets.test.ts
@@ -3,19 +3,20 @@ import { AugmentedEvent, SubmittableExtrinsic } from '@polkadot/api/types'
 import { KeyringPair } from '@polkadot/keyring/types'
 import { Vec } from '@polkadot/types'
 import { EventRecord, ExtrinsicStatus, Hash } from '@polkadot/types/interfaces'
-import { Codec } from '@polkadot/types/types'
-import { combineLatest, concat, EMPTY, firstValueFrom, map, Observable, of, switchMap, tap } from 'rxjs'
+import { combineLatest, concat, EMPTY, firstValueFrom, from, map, max, Observable, of, switchMap, tap } from 'rxjs'
 
 import { Asset } from '../src'
 import { waitFor } from './utils/waitFor'
 import { ALICE } from './consts'
+import {PalletAssetsAssetDetails} from "@polkadot/types/lookup";
 
+const TIMEOUT = 100000
 export function aliceSigner(): KeyringPair {
   const keyring = new Keyring({ type: 'sr25519' })
 
   return keyring.addFromUri('//Alice')
 }
-export async function execute(extrinsic: SubmittableExtrinsic<'rxjs'>, singer: KeyringPair, logger = { info: console.log }): Promise<void> {
+export async function execute(api: ApiRx, extrinsic: SubmittableExtrinsic<'rxjs'>, singer: KeyringPair, logger = { info: console.log }): Promise<void> {
   let currentTxDone = false
 
   function sendStatusCb({ events = [], status }: { events?: EventRecord[], status: ExtrinsicStatus; }) {
@@ -34,8 +35,25 @@ export async function execute(extrinsic: SubmittableExtrinsic<'rxjs'>, singer: K
         ({ event }) => {
           if (event.method === 'ExtrinsicSuccess') {
             logger.info('Transaction succeeded')
-          } else if (event.method === 'ExtrinsicFailed') {
+          } else if (api.events.system.ExtrinsicFailed.is(event)) {
             logger.info('Transaction failed')
+            const [dispatchError] = event.data
+            let errorInfo
+
+            // decode the error
+            if (dispatchError.isModule) {
+              // for module errors, we have the section indexed, lookup
+              // (For specific known errors, we can also do a check against the
+              // api.errors.<module>.<ErrorName>.is(dispatchError.asModule) guard)
+              const decoded = api.registry.findMetaError(dispatchError.asModule)
+
+              errorInfo = `${decoded.section}.${decoded.name}`
+            } else {
+              // Other, CannotLookup, BadOrigin, no extra info
+              errorInfo = dispatchError.toString()
+            }
+
+            logger.info(`:: ExtrinsicFailed:: ${errorInfo}`)
           }
         }
       )
@@ -43,8 +61,9 @@ export async function execute(extrinsic: SubmittableExtrinsic<'rxjs'>, singer: K
     }
   }
 
-  await firstValueFrom(extrinsic.signAndSend(singer, sendStatusCb))
-  await waitFor(() => currentTxDone, { timeout: 20000 })
+  const a = extrinsic.signAndSend(singer)
+  a.subscribe(sendStatusCb)
+  await waitFor(() => currentTxDone, { timeout: TIMEOUT })
 }
 
 type EventCheck = AugmentedEvent<'promise'> | false | undefined | null;
@@ -58,41 +77,56 @@ function extractEvents(eventRecords: Vec<EventRecord>, checks: EventCheck[]): Ob
     return of(eventRecords.createdAtHash)
   }
 
-  return of()
+  return EMPTY
 }
 
 describe('derive assets', () => {
   it('updates on create asset event', async () => {
-    const apiRx = new ApiRx({ provider: new WsProvider('ws://127.0.0.1:9988') })
-    await firstValueFrom(apiRx.isReady)
-    const checks = [apiRx.events.assets.Created, apiRx.events.assets.Destroyed]
-    const currentBlockNumber = await firstValueFrom(apiRx.query.system.number())
-    const currentBlockHash = await firstValueFrom(apiRx.query.system.blockHash(currentBlockNumber.subn(1)))
-
-    const project = (events: Vec<EventRecord>) => extractEvents(events, checks)
-    const resultObservable = concat(of(currentBlockHash), apiRx.query.system.events().pipe(switchMap(project)))
-      .pipe(tap((b) => console.log(b)))
+    const api = new ApiRx({ provider: new WsProvider('ws://127.0.0.1:9988') })
+    await firstValueFrom(api.isReady)
+    const checks = [api.events.assets.Created, api.events.assets.Destroyed]
+    const { hash: currentBlockHash } = await firstValueFrom(api.rpc.chain.subscribeNewHeads())
+    const resultObservable = concat(of(currentBlockHash as Hash), api.query.system.events()
+      .pipe(switchMap((events: Vec<EventRecord>) => extractEvents(events, checks))))
+      .pipe(tap((blockHash) => console.log(`Block hash: ${blockHash}`)))
       .pipe(
-        switchMap((blockHash: Hash) => apiRx.query.assets.asset.keysAt(blockHash))
-      )
-      .pipe(
-        switchMap((keys) => {
-          const ids = keys.map(({ args: [id] }) => id)
-
-          return combineLatest([
-            apiRx.query.assets.asset.multi(ids),
-            apiRx.query.assets.metadata.multi(ids)
+        switchMap((blockHash: Hash) =>
+          combineLatest([
+            api.query.assets.asset.entriesAt(blockHash),
+            api.query.assets.metadata.entriesAt(blockHash),
+            api.query.assets.asset.keysAt(blockHash)
           ])
-        })
+        )
       )
-      .pipe(map(([maybeAssets, maybeMetadatas]) => {
+      // .pipe(tap(keys => console.log(`Keys: ${keys.map(k => k.toHuman())}`)))
+      // .pipe(
+      //   switchMap((blockHash: Hash) => api.query.assets.asset.keysAt(blockHash))
+      // )
+      // // .pipe(tap(keys => console.log(`Keys: ${keys.map(k => k.toHuman())}`)))
+      // .pipe(
+      //   switchMap((keys) => {
+      //     const ids = keys.map(({ args: [id] }) => id)
+      //
+      //     return combineLatest([
+      //       api.query.assets.asset.entries(ids),
+      //       api.query.assets.metadata.multi(ids),
+      //       of(ids)
+      //     ])
+      //   })
+      // )
+      .pipe(tap(([maybeAssets, maybeMetadatas]) => {
+        console.log(`assets = ${maybeAssets.length}\nmetas = ${maybeMetadatas.length}`)
+      }))
+      .pipe(map(([maybeAssets, maybeMetadatas, ids]) => {
         const result: Asset[] = []
 
         maybeAssets.forEach((asset, index) => {
+          console.log(`For id ${ids[index]} there is ${asset.isSome ? 'an' : 'no'} asset and ${maybeMetadatas[index].decimals.gtn(0) ? 'a' : 'no'} metadata`)
           if (asset.isSome) {
             result.push({
               ...asset.unwrap(),
-              ...maybeMetadatas[index]
+              ...maybeMetadatas[index],
+              id: ids[index]
             } as unknown as Asset)
           }
         })
@@ -102,11 +136,17 @@ describe('derive assets', () => {
 
     resultObservable.subscribe(res => console.log(JSON.stringify(res, null, 2)))
 
-    const extrinsic = apiRx.tx.assets.create(14, ALICE, 300)
+    const lastAssetId = await firstValueFrom(api.query.assets.asset.keys().pipe(map(k => k.length)))
+    const assetId = lastAssetId + 1
+    const extrinsic = api.tx.utility.batchAll([
+      api.tx.assets.create(assetId, ALICE, 30000000),
+      api.tx.assets.setMetadata(assetId, `test${assetId}`, `TEST${assetId}`, 5)
+    ])
     console.log('Sending extrinsic')
-    await execute(extrinsic, aliceSigner())
+    await execute(api, extrinsic, aliceSigner())
     console.log('Executed')
-  }, 100000)
+    await waitFor(() => false, { timeout: TIMEOUT })
+  }, TIMEOUT)
 })
 //
 // function convertAssetMetadata(fetchedMeta: PalletAssetsAssetMetadata): AssetMeta {
